7e84cc9396684ea1c51bd62cb98fa495fa273150
Andres Orrego <andresorregolopez@gmail.com>
2024-04-04 14:43:46 -0500

    chore: added imports and IFormattedItem interface

--- src/webparts/birthday/components/Birthday.tsx
+++ src/webparts/birthday/components/Birthday.tsx
@@ -1,13 +1,35 @@
 import * as React from 'react';
 import styles from './Birthday.module.scss';
 import type { IBirthdayProps } from './IBirthdayProps';
-//import { escape } from '@microsoft/sp-lodash-subset';
+import { useState,useEffect } from 'react';
+import { spfi, SPFx } from '@pnp/sp';
+import "@pnp/sp/webs";
+import "@pnp/sp/site-users/web";
+import "@pnp/sp/files";
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+import "@pnp/sp/items/get-all";
+import "@pnp/sp/fields";
+import { WebPartContext } from '@microsoft/sp-webpart-base';
+//import { escape } from '@microsoft/sp-lodash-subset'; 
+
+export interface IFormattedItem {
+  ID: number;
+  Date: string;
+  MonthNumber: number;
+  MonthName: string;
+  Day:number;
+  User: {
+    Title: string;
+    EMail:string
+  }
+}
 
 const Birthday: React.FC<IBirthdayProps> =(props)=> {
 
-    return (
-      <p className={styles.mainText}>hi</p>
-    );
+  return (
+    <p className={styles.mainText}>hi</p>
+  );
 }
 
 export default Birthday
